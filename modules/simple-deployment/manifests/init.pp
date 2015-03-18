5728d9788bd45547ee256f9de20c12c93f9108da
mcneil-hack <ahendley@internode.on.net>
2015-03-18 21:42:31 +1100

    logging of output take 2

--- modules/simple-deployment/manifests/init.pp
+++ modules/simple-deployment/manifests/init.pp
@@ -10,8 +10,7 @@ class simple-deployment {
 		command => '/usr/local/bin/bundle install',
 		user => "root",
 		group => "root",
-		output => true,
-		logoutput => on_failure,
+		logoutput => true,
 		unless => '/usr/local/bin/bundle check',
 		environment => "HOME=/root",
 		require => [
