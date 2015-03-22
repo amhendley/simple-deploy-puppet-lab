e073ebb7985f29eaf9616f910d59604da423043c
mcneil-hack <ahendley@internode.on.net>
2015-03-22 19:09:55 +1100

    forcing install of dependant ruby gem packages

--- modules/ruby/manifests/init.pp
+++ modules/ruby/manifests/init.pp
@@ -9,5 +9,29 @@ class ruby {
 		provider => 'gem',
 		require => Package['ruby2.0'],
 	}
+
+	package { 'ruby-rack':
+		ensure   => 'installed',
+		provider => 'gem',
+		require => Package['ruby2.0'],
+	}
+
+	package { 'ruby-rack-protection':
+		ensure   => 'installed',
+		provider => 'gem',
+		require => Package['ruby2.0'],
+	}
+
+	package { 'ruby-tilt':
+		ensure   => 'installed',
+		provider => 'gem',
+		require => Package['ruby2.0'],
+	}
+
+	package { 'ruby-sinatra':
+		ensure   => 'installed',
+		provider => 'gem',
+		require => Package['ruby2.0'],
+	}
 	
 }
